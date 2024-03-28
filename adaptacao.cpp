5cc8425d0a5c1f0252838c17d1cc1ad8457af610
L3xTech <l3xtech@tuta.io>
2024-03-28 13:02:53 -0300

    adapt2

--- adaptacao.cpp
+++ adaptacao.cpp
@@ -10,3 +10,23 @@ std::string Log4Data::constructStructuredMessage(const std::string& type, const 
     }
     return ss.str();
 }
+
+
+void Log4Data::logInformation(const logDataInput& value) {
+    std::stringstream ss;
+    ss << "LMESSAGE_ID=" << value._message_id << "|"
+       << "LEVENT=" << value._event << "|"
+       << "LALGORITHM=" << value._algorithm << "|"
+       << "LSOURCE=" << value._source << "|"
+       << "LTARGET=" << value._target << "|"
+       << "LRISK_ENGINE=" << value._risk_engine << "|"
+       << "LSCENARIO_VERSION=" << value._scenario_version << "|"
+       << "LSCENARIO_QTY=" << value._scenario_qty << "|"
+       << "LREGULAR_POSITIONS_QTY=" << value._regular_positions_qty << "|"
+       << "LCOLLATERAL_QTY=" << value._collateral_qty << "|"
+       << "LOPTION_FLEX_QTY=" << value._option_flex_qty << "|"
+       << "LSWAP_QTY=" << value._swap_qty << "|"
+       << "LTOTAL_POSITION=" << value._total_positions_qty << "|"
+       << "LCALCULATION_TYPE=" << value._calculationType;
+    CoreLog::info(ss.str());
+}
